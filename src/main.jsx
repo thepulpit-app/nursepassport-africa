53f2742acefb574adbacb3cef0d53c42a495872c
Temitope Ajijola | Head Business Program TemitopeAjijolaHeadB IT <temitope.ajijola@flyvaluejet.com>
2026-06-13 11:14:11 +0100

    Add global dark mode support with mobile toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/darkmode.css'
 import { inject } from "@vercel/analytics"
 inject()
 import App from './App.jsx'
